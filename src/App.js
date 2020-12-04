b0ae6488421cfe5c740cde02898bf119e1704bf5
Ivo Junior <maciel.jr95@gmail.com>
2020-12-04 14:29:27 -0300

    Application finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [location, setLocation] = useState(false);
+  const [weather, setWeather] = useState(false);
+
+  let getWeather = async (lat, long) => {
+    let res = await axios.get("https://api.openweathermap.org/data/2.5/weather", {
+      params: {
+        lat: lat,
+        lon: long,
+        appid: process.env.REACT_APP_OPEN_WHEATHER_KEY,
+        lang: 'pt',
+        units: 'metric'
+      }
+    });
+    setWeather(res.data);
+  }
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position)=> {
+      getWeather(position.coords.latitude, position.coords.longitude);
+      setLocation(true)
+    })
+  }, [])
+
+  if (location == false){
+    return (
+      <>
+        Você precisa habilitar a localização no browser
+      </>
+    )
+  } else if (weather == false) {
+    return (
+      <>
+        Carregando o clima...
+      </>
+    )
+  } else {
+    return (
+      <>
+        <h3>Clima nas suas Coordenadas ({weather['weather'][0]['description']})</h3>
+        <hr />
+        <ul>
+          <li>Temperatura atual: {weather['main']['temp']}°</li>
+        </ul>
+      </>
+    );
+  }
 }
 
 export default App;
